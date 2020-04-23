f13515bdec49b4bac73e9f59101112644ba58f67
SansaSt <sc.comsa@mail.ru>
2020-04-23 13:30:26 +0500

    lesson1313

--- script/script.js
+++ script/script.js
@@ -1,78 +1,80 @@
 'use strict';
 
-const todoControl = document.querySelector('.todo-control'),
-      headerInput = document.querySelector('.header-input'),
-      todoList = document.querySelector('.todo-list'),
-      todoCompleted = document.querySelector('.todo-completed');
+const todoControl = document.querySelector('.todo-control');
+const headerInput = document.querySelector('.header-input');
+const headerButton = document.querySelector('.header-button');
+const todoList = document.querySelector('.todo-list');
+const todoCompleted = document.querySelector('.todo-completed');
 
-let todoData = [];
 
-const addData = function() {
-      localStorage.Value = JSON.stringify(todoData);
-      };
-       // Массив для хранения данных (дел)
+// 7) Дела из localStorage подгружаться должны автоматически при загрузки странице
+let todoData = JSON.parse(localStorage.getItem('newTodo'));
+if (todoData === null) {
+  todoData = [];
+} else {
+  todoData = JSON.parse(localStorage.getItem('newTodo'));
+}
 
-const render = function(){ // функция добалвяет дела на страницу
-    todoList.textContent = ''; // присваиваем пустые строки
-    todoCompleted.textContent = ''; 
+const render = function() {
+  todoList.textContent = '';
+  todoCompleted.textContent = '';
 
-    localStorage.setItem('newTodo', JSON.stringify(todoData)); // Сохранять данные о делах в localStorage
+  // 6) Сохранять данные о делах в localStorage (советую в виде массива)
+  localStorage.setItem('newTodo', JSON.stringify(todoData));
 
-  todoData.forEach(function(item){ // перебор массива с данными
-    const li = document.createElement('li'); // создание элемента
-          li.classList.add('todo-item'); // добавление ему класса
+  todoData.forEach(function(item, i) {
+    const li = document.createElement('li');
+    li.classList.add('todo-item');
 
-          li.innerHTML = '<span class="text-todo">' + item.value + '</span>' +
-          '<div class="todo-buttons">' +
-            '<button class="todo-remove"></button>' +
-            '<button class="todo-complete"></button>' +
-          '</div>';    // добавление верстки
-          
-          if(item.complited) {
-            todoCompleted.append(li);
-          } else {
-            todoList.append(li);
-          }
+    li.innerHTML = '<span class="text-todo">' + item.value + '</span>' +
+    '<div class="todo-buttons">' +
+      '<button class="todo-remove"></button>' +
+      '<button class="todo-complete"></button>' +
+    '</div>';
 
-      const btnTodoComplete = li.querySelector('.todo-complete');
+    if (item.complited) {
+      todoCompleted.append(li);
+    } else {
+    todoList.append(li);
+    }
 
-      btnTodoComplete.addEventListener('click', function(){
-      item.complited = !item.complited; // инвертация
-
+    const btnTodoComplete = li.querySelector('.todo-complete');
+    btnTodoComplete.addEventListener('click', function() {      
+      item.complited = !item.complited;
       render();
-      
     });
 
-      const todoRemove = li.querySelector('.todo-remove'); //  Удаление дел на кнопку КОРЗИНА
-            todoRemove.addEventListener('click', function(){
-            todoData.splice(item, 1);
+    // // 5) Удаление дел на кнопку КОРЗИНА
+    const todoRemove = li.querySelector('.todo-remove');
+    todoRemove.addEventListener('click', function() {
+      todoData.splice(i, 1);
 
-            render();
-            });
-
+      render();
+    });
+    
   });
 
-  headerInput.value = ''; // Поле ввода после добавления дела должно очищаться
+  //4) Поле ввода после добавления дела должно очищаться
+  headerInput.value = '';
 };
 
 
-todoControl.addEventListener('submit', function(event){
-  
+
+todoControl.addEventListener('submit', function(event) {
   event.preventDefault();
 
-  if (headerInput.value === '') { return; } // Пустые дела добавляться не должны
+  // 3) Пустые дела добавляться не должны
+  if (headerInput.value === '') { return; }
 
   const newTodo = {
-        value: headerInput.value,
-        complited: false
+    value: headerInput.value,
+    complited: false,
   };
 
   todoData.push(newTodo);
+    //6) Сохранять данные о делах в localStorage (советую в виде массива)
+  localStorage.setItem('newTodo', JSON.stringify(todoData));
 
   render();
-
 });
-
 render();
-
-localStorage.clear();
